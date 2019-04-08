6ea54fd7ba1edc9d9a1e80b03a889c18ddcbb7d7
mhmerric <mhmerric@email.uark.edu>
2019-04-08 12:05:14 -0500

    content_shopping_cart changes

--- app/src/main/java/edu/uark/uarkregisterapp/ShoppingCartActivity.java
+++ app/src/main/java/edu/uark/uarkregisterapp/ShoppingCartActivity.java
@@ -1,4 +1,38 @@
 package edu.uark.uarkregisterapp;
 
-public class ShoppingCartActivity {
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.uark.uarkregisterapp.adapters.ProductListAdapter;
+import edu.uark.uarkregisterapp.models.api.ApiResponse;
+import edu.uark.uarkregisterapp.models.api.Product;
+import edu.uark.uarkregisterapp.models.api.services.ProductService;
+import edu.uark.uarkregisterapp.models.transition.ProductTransition;
+
+public class ShoppingCartActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_products_listing);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+
+        ActionBar actionBar = this.getSupportActionBar();
+        if (actionBar != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+    }
+
 }
